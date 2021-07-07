3d40150fea8973e6dbb8d54e2b788ae169223b3b
thehienvnag <thehienvnag@gmail.com>
2021-07-07 22:34:23 +0700

    demo/HienHT/commit-code-demo

--- src/App/Routes/appRoutes.js
+++ src/App/Routes/appRoutes.js
@@ -1,3 +1,4 @@
+import TestPage from "App/Containers/Admin/TestPage";
 import DashboardLayout from "../Components/DashboardLayout/DashboardLayout";
 import AdHomePage from "../Containers/Admin/HomePage/index";
 import BmHomePage from "../Containers/BuildingManager/HomePage/index";
@@ -8,7 +9,8 @@ const appRoutes = [
     element: <DashboardLayout />,
     children: [
       { path: "/test", element: <BmHomePage /> },
-      { path: "/test", element: <BmHomePage /> },
+      { path: "/test2", element: <BmHomePage /> },
+      { path: "/testPage", element: <TestPage /> },
     ],
   },
   {
@@ -16,7 +18,7 @@ const appRoutes = [
     element: <DashboardLayout />,
     children: [
       { path: "/test", element: <AdHomePage /> },
-      { path: "/test", element: <AdHomePage /> },
+      { path: "/test2", element: <AdHomePage /> },
     ],
   },
 ];
