8a76bc29f270270502d6733756135c264765bd97
thehienvnag <thehienvnag@gmail.com>
2021-07-08 23:18:35 +0700

    enhance/HienHT/Add-comment for component

--- src/App/Containers/BuildingManager/FloorPlanPage/Header/index.jsx
+++ src/App/Containers/BuildingManager/FloorPlanPage/Header/index.jsx
@@ -65,6 +65,13 @@ const routes = [
   },
 ];
 
+/**
+ * Page wrapper for new page
+ * @param {object} [params] parameters for get request
+ * @param {PropTypes.func} [params.handleCreate] current page of get request
+ * @param {PropTypes.func} [params.handleDelete] current page size of get request
+ * @param {PropTypes.func} [params.handleRefresh] building id which contains floor plans
+ */
 const Header = ({ handleCreate, handleDelete, handleRefresh }) => (
   <PageHeader
     title="FLOOR PLANS"
