9de32b8d70a3b5b86a838c8a795210cb16fbf526
Đang truy cập <ang truy cp>
2021-07-15 09:10:13 +0700

    feature/LongDH/Update code create building

--- src/App/Services/account.service.js
+++ src/App/Services/account.service.js
@@ -1,5 +1,5 @@
-import axiosClient from "../Utils/axiosClient";
 import { accounts } from "../Constants/endpoints";
+import axiosClient, { postFormData } from "../Utils/axiosClient";
 /**
  * Page wrapper for new page
  * @param {object} [params] parameters for get request
@@ -17,4 +17,30 @@ export const getAccountsPaging = async ({ pageIndex = 1, pageSize = 5 }) => {
   console.log(params);
   const response = await axiosClient.get(accounts, { params });
   return response.data;
+};
+
+/**
+ * Page wrapper for new page
+ * @param {object} [data] values post
+ */
+export const postAccount = async (data) => {
+  try {
+    const dataPost = await postFormData(accounts, data);
+    return dataPost;
+  } catch (error) {
+    console.log(error?.message);
+  }
+};
+
+/**
+ * Page wrapper for new page
+ * @param {object} [data] values post
+ */
+export const testPoseWithoutFile = async (data) => {
+  try {
+    const response = await axiosClient.post(accounts, data);
+    return response.data;
+  } catch (error) {
+    console.log(error?.message);
+  }
 };
