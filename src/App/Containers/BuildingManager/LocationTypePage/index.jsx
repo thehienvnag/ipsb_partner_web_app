730ab83c76c1b182e02946fe55ac01b4327e4ba3
Cao Quynh Trang <trangcaoquynh1207@gmail.com>
2021-07-19 15:50:44 +0700

    feature/TrangCQ/code-create-places-list-screen

--- src/App/Containers/BuildingManager/LocationTypePage/index.jsx
+++ src/App/Containers/BuildingManager/LocationTypePage/index.jsx
@@ -4,24 +4,27 @@ import Header from "./Header/index";
 import { Card } from "antd";
 import { PageWrapper, PageBody } from "App/Components/PageWrapper";
 import LocationTypeTable from "./Contents/LocationTypeTable/index";
-
-// import CreateStoreModal from "./Contents/CreateStoreModal";
+import CreateLocationTypeModel from "./Contents/CreateLocationTypeModel";
 
 const LocationTypePage = () => {
-  //   const [visible, setVisible] = useState(false);
-  //   const handleCreate = () => {
-  //     setVisible(true);
-  //   };
-
+  const [visible, setVisible] = useState(false);
+  const handleCreate = () => {
+    setVisible(true);
+  };
+  const handleCancel = () => {
+    setVisible(false);
+  };
   return (
     <PageWrapper>
-      <Header
-      //   handleCreate={handleCreate}
-      />
+      <Header handleCreate={handleCreate} />
       <PageBody>
         <Card>
           <LocationTypeTable />
         </Card>
+        <CreateLocationTypeModel
+          visible={visible}
+          handleCancel={handleCancel}
+        />
       </PageBody>
     </PageWrapper>
   );
